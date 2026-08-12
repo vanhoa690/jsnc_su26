8ecdf36d4f5f6b78db6dba6b24cf58187201fc59
vanhoa690 <vanhoa690@gmail.com>
2026-08-12 14:51:46 +0700

    edit html

---
+++ edit.js
@@ -0,0 +1,18 @@
+console.log("add them");
+
+// addStudent();
+document.getElementById("form-add").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+
+  axios
+    .post("http://localhost:3000/students", {
+      name: name,
+      age: age,
+      email: "hoadv@gmail.com",
+    })
+    .then(() => {
+      alert("them thanh cong");
+    });
+});
